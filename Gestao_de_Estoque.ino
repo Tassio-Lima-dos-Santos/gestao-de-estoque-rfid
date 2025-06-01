9be13c2afeefea2591aca8d17129b13cc65bddbd
Tassio Lima dos Santos <159482218+Tassio-Lima-dos-Santos@users.noreply.github.com>
2025-06-01 00:56:17 -0300

    Criação de variáveis e funções preliminares
    Criação da variável global status para indicar o status do último comando realizado pelo módulo, e criação da função dump_byte_array, função essa que facilita a visualização de array de bytes, que pode vir por exemplo da leitura de um bloco do PICC.

--- Gestao_de_Estoque.ino
+++ Gestao_de_Estoque.ino
@@ -1,15 +1,24 @@
-/*#include <SPI.h>
+#include <SPI.h>
 #include <MFRC522.h>
 
 #define SS_PIN 10
 #define RST_PIN 5
 
+// Instanciando o objeto
 MFRC522 rfid(SS_PIN, RST_PIN);
+
+// Variáveis globais
+MFRC522::StatusCode status;
+
+// Declaração de funções úteis
+void dump_byte_array(byte *buffer, byte bufferSize);
+// void dump_string(byte *buffer, byte bufferSize); Ao invés de criar uma função inútil que nem um otário, só use Serial.write(byte *buffer, int size)
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);
   SPI.begin(); // init SPI bus
-  rfid.PCD_Init(); // init MFRC522
+  rfid.PCD_Init(); // iniciando o módulo MFRC522
 }
 
 void loop() {
@@ -21,14 +30,22 @@ void loop() {
 
       // print NUID in Serial Monitor in the hex format
       Serial.print("UID:");
-      for (int i = 0; i < rfid.uid.size; i++) {
+      dump_byte_array(rfid.uid.uidByte, (byte)rfid.uid.size);
+      /*for (int i = 0; i < rfid.uid.size; i++) {
         Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
         Serial.print(rfid.uid.uidByte[i], HEX);
-      }
+      }*/
       Serial.println();
 
       rfid.PICC_HaltA(); // halt PICC
       rfid.PCD_StopCrypto1(); // stop encryption on PCD
     }
   }
-}*/
+}
+
+void dump_byte_array(byte *buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+        Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+        Serial.print(buffer[i], HEX);
+    }
+}
