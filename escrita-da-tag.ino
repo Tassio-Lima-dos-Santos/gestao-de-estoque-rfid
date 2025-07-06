f8b3b147fd56776c05cc09b06f50a0e16fe78623
Tassio Lima dos Santos <159482218+Tassio-Lima-dos-Santos@users.noreply.github.com>
2025-07-06 13:04:02 -0300

    Update escrita-da-tag.ino

--- escrita-da-tag.ino
+++ escrita-da-tag.ino
@@ -38,33 +38,41 @@ void loop() {
   Serial.println(F("Digite o nome do material"));
   readSerial();
   sector = secaoNome;
-  if(write_on_sector(input, 48)) return;
+  if(write_on_sector(input, 48)){
+    quitComm();
+    return;
+  }
 
   Serial.println(F("Digite o lote do material"));
   readSerial();
   sector = secaoLote;
-  if(write_on_sector(input, 48)) return;
+  if(write_on_sector(input, 48)){
+    quitComm();
+    return;
+  }
 
   Serial.println(F("Digite o fornecedor do material"));
   readSerial();
   sector = secaoFornecedor;
-  if(write_on_sector(input, 48)) return;
+  if(write_on_sector(input, 48)){
+    quitComm();
+    return;
+  }
 
   Serial.println(F("Escrita concluída!"));
   Serial.println();
 
-  mfrc522.PICC_HaltA();
-  mfrc522.PCD_StopCrypto1();
+  quitComm();
 }
 
-void dump_byte_array(byte *buffer, byte bufferSize) {
+void dump_byte_array(byte *buffer, byte bufferSize) { // Função para printar um buffer de bytes no monitor serial na forma hexadecimal.
     for (byte i = 0; i < bufferSize; i++) {
         Serial.print(buffer[i] < 0x10 ? " 0" : " ");
         Serial.print(buffer[i], HEX);
     }
 }
 
-int authenticate(){
+int authenticate(){ // Função de autenticação do PCD com o PICC, é necessário chamar essa função antes de fazer qualquer operação no PICC. A função retorna 0 se der certo e 1 se der errado.
   status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, &key, &(mfrc522.uid));
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("PCD_Authenticate() failed: "));
@@ -74,7 +82,7 @@ int authenticate(){
   return 0;
 }
 
-int write_on_block(byte *buffer, byte bufferSize){
+int write_on_block(byte *buffer){ // Função para escrever o buffer passado no argumento no bloco endereçado pela variável global block. Retorna 0 se der certo e 1 se der errado.
   if(authenticate()) return 1;
   status = mfrc522.MIFARE_Write(block, buffer, 16);
   if (status != MFRC522::STATUS_OK) {
@@ -85,18 +93,18 @@ int write_on_block(byte *buffer, byte bufferSize){
   return 0;
 }
 
-int write_on_sector(byte *buffer, byte bufferSize){
+int write_on_sector(byte *buffer){ // Função para escrever o buffer passado no argumento no setor endereçado pela variável global sector. Retorna 0 se der certo e 1 se der errado.
   for(int i = 0; i < 3; i++){
     block = sector*4+i;
-    if(write_on_block(buffer+i*16, 16)){
-      Serial.println("ERRO NA ESCRITA!!!");
+    if(write_on_block(buffer+i*16)){
+      Serial.println(F("ERRO NA ESCRITA!!!"));
       return 1;
     }
   }
   return 0;
 }
 
-int read_block(byte *buffer, byte bufferSize){
+int read_block(byte *buffer, byte bufferSize){ // Função para ler o bloco endereçado pela variável global block e salvar no buffer passado no argumento. Retorna 0 se der certo e 1 se der errado.
   if(authenticate()) return 1;
   status = mfrc522.MIFARE_Read(block, buffer, &bufferSize);
   if (status != MFRC522::STATUS_OK) {
@@ -107,18 +115,95 @@ int read_block(byte *buffer, byte bufferSize){
   return 0;
 }
 
-int read_section(byte *buffer, byte bufferSize){
+int read_section(byte *buffer){ // Função para ler o setor endereçado pela variável global sector e salvar no buffer passado no argumento. Retorna 0 se der certo e 1 se der errado.
   for(int i = 0; i < 3; i++){
     block = sector*4+i;
     if(read_block(buffer+i*16, 18)){
-      Serial.println("ERRO NA LEITURA!!!");
+      Serial.println(F("ERRO NA LEITURA!!!"));
       return 1;
     }
   }
   return 0;
 }
 
-void bufferClean(){
+int setValue_block(int value){
+  if(authenticate()) return -1;
+  status = mfrc522.MIFARE_SetValue(block, value);
+  if (status != MFRC522::STATUS_OK) {
+    Serial.print(F("MIFARE_SetValue() failed: "));
+    Serial.println(mfrc522.GetStatusCodeName(status));
+    return -1;
+  }
+  status = mfrc522.MIFARE_GetValue(block, &value);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_GetValue() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  return value;
+}
+
+int readValue_block(){
+  int32_t value;
+  if(authenticate()) return -1;
+  status = mfrc522.MIFARE_GetValue(block, &value);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_GetValue() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  return value;
+}
+
+int increment_block(int inc){ // Função para incrementar o argumento passado no bloco endereçado pela variável block. Retorna o valor salvo no bloco após o incremento se der certo e -1 se der errado.
+  int32_t value;
+  if(authenticate()) return -1;
+  status = mfrc522.MIFARE_Increment(block, inc);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_Increment() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  status = mfrc522.MIFARE_Transfer(block);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_Transfer() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  status = mfrc522.MIFARE_GetValue(block, &value);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_GetValue() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  return value;
+}
+
+int decrement_block(int inc){ // Função para decrementar o argumento passado no bloco endereçado pela variável block. Retorna o valor salvo no bloco após o decremento se der certo e -1 se der errado.
+  int32_t value;
+  if(authenticate()) return -1;
+  status = mfrc522.MIFARE_Decrement(block, inc);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_Decrement() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  status = mfrc522.MIFARE_Transfer(block);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_Transfer() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  status = mfrc522.MIFARE_GetValue(block, &value);
+  if (status != MFRC522::STATUS_OK) {
+      Serial.print(F("MIFARE_GetValue() failed: "));
+      Serial.println(mfrc522.GetStatusCodeName(status));
+      return -1;
+  }
+  return value;
+}
+
+void bufferClean(){ // Função para limpar o array input.
   for(int i = 0; i < 48; i++){
     input[i] = 0;
   }
